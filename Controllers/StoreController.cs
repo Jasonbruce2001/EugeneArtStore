cfcc6205eeb48575e596265a2201b8010fce03f0
Jason Bruce <46138154+Jasonbruce2001@users.noreply.github.com>
2025-03-05 14:36:41 -0800

    Store update

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -1,12 +1,26 @@
+using EugeneArtStore.Data;
+using EugeneArtStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EugeneArtStore.Controllers;
 
 public class StoreController : Controller
 {
-    // GET
+    private readonly ApplicationDbContext _context;
+
+    public StoreController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> model = _context.Products.ToList();
+        return View(model);
+    }
+
+    public IActionResult Details(int id)
+    {
+        Product model = _context.Products.Find(id);
+        return View(model);
     }
 }
